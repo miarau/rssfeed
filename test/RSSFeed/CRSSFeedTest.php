cb95f96b4e28c7c2d3403e05a9fd92ac7d10945f
Mia Raunegger <mia@design4u2.se>
2016-04-27 18:30:34 +0200

    New CRSSFeedtest

--- test/RSSFeed/CRSSFeedTest.php
+++ test/RSSFeed/CRSSFeedTest.php
@@ -8,25 +8,25 @@ namespace Miax\RSSFeed;
 use SimplePie;
 
 //change the cache directory to wherever you want it
-//define('CACHE', __DIR__.'/../webroot/cache/');
+define('CACHE', __DIR__.'/../webroot/cache/');
 
-//require_once(__DIR__.'/../simplepie/simplepie_1.3.1.mini.php');
-//require_once(__DIR__.'/../CRSSFeed.php');
+require_once(__DIR__.'/../simplepie/simplepie_1.3.1.mini.php');
+require_once(__DIR__.'/../CRSSFeed.php');
 
 class CRSSFeedTest extends \PHPUnit_Framework_TestCase {
    
    private $feed;
    
-   public function testTesting() {
-    $this->assertEquals(1, 2, "Just making sure this is being checked.");
-  }
+  /* public function testTesting() {
+    $this->assertEquals(1, 1, "Just making sure this is being checked.");
+  }*/
    
-   /*public function setUp(){
+   public function setUp(){
 	   $this->feed = new \Miax\RSSFeed\CRSSFeed(['http://feeds.feedburner.com/idg/vzzs']);
 	   $test = get_items($this->feed);
 	   $exp = "\Miax\RSSFeed\CRSSFeed";
 	   $this->assertEquals($test, $exp, "Wrong items");
-   }*/
+   }
    
   /**
      * Test 
